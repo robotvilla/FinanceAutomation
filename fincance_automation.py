eb0a5da5c0ba7dc680302f1ef23de1bd12827dc3
Robert <r.villazana@yahoo.com>
2020-04-22 19:40:00 -0500

    displaying spendings and earnings

--- fincance_automation.py
+++ fincance_automation.py
@@ -1,6 +1,8 @@
 import csv
 
-
+spendings = 0
+earnings = 0
+statements = []
 
 with open('statements.CSV') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
@@ -8,9 +10,22 @@ with open('statements.CSV') as csvfile:
         row = row[:-1]
         row.pop(0)
         row.pop(3)
-        
+
         #remove trailing element if it is empty
         while not row[-1]:
             row.pop()
+        statements.append(row)
+    
+    print(statements[0])
+    statements.pop(0)
+    for statement in statements:
+        print(statement)
 
-        print(row)
+        spending_or_earning = float(statement[2])
+        if spending_or_earning < 0:
+            spendings = spendings + spending_or_earning
+        elif spending_or_earning > 0:
+            earnings = earnings + spending_or_earning
+
+    print("Spending: {0:.2f}".format(spendings))
+    print("Earnings: {0:.2f}".format(earnings))
